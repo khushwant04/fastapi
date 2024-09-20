1eea4723294170644fa1ab2588e261c06befb334
khushwant04 <khushwantsanwalot2004@outlook.com>
2024-09-20 16:54:59 +0000

    commit

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from json import load
+from pyexpat import model
 from typing import Optional
 from fastapi import Depends, FastAPI , Response, status, HTTPException
 from pydantic import BaseModel
@@ -34,7 +35,7 @@ class Post(BaseModel):
     title: str 
     content: str
     published: bool = True 
-    rating: Optional[int] = None
+    # rating: Optional[int] = None
 
 while True:
     try:
@@ -60,32 +61,40 @@ def index():
 
 @app.get("/sqlalchemy")
 def test_posts( db: Session = Depends(get_db)):
-    return {"status":"success"}
-    pass
+    posts = db.query(models.Post).all()
+
+    return {"data":posts}
+    
 
 
 @app.get("/posts")
-def get_posts():
-    cursor.execute("""Select * from posts""")
-    posts = cursor.fetchall()
+def get_posts(db: Session = Depends(get_db)):
+    # cursor.execute("""Select * from posts""")
+    # posts = cursor.fetchall()
+    posts = db.query(models.Post).all()
     return {"data":posts}
 
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_posts(post: Post):
-    cursor.execute("""INSERT INTO posts (title, content, published) VALUES(%s,%s,%s) RETURNING * """, (post.title, post.content, post.published))
-    new_post = cursor.fetchone()
-    conn.commit()
+def create_posts(post: Post, db: Session = Depends(get_db)):
+    # cursor.execute("""INSERT INTO posts (title, content, published) VALUES(%s,%s,%s) RETURNING * """, (post.title, post.content, post.published))
+    # new_post = cursor.fetchone()
+    # conn.commit()
+    new_post = models.Post(**post.dict())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
     return {"message": new_post}
 
 
 @app.get("/posts/{id}")
-def get_post(id: int, response: Response):
-    cursor.execute(f"""SELECT * FROM posts WHERE id = %s""",(str(id)))
-    post = cursor.fetchone()
-    if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"post with {id} not found")
+def get_post(id: int,db: Session = Depends(get_db)):
+    # cursor.execute(f"""SELECT * FROM posts WHERE id = %s""",(str(id)))
+    # post = cursor.fetchone()
+    # if not post:
+    #     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+    #                         detail=f"post with {id} not found")
+    post = db.query(models.Post).filter(models.Post.id == id).all()
     return {"data": post}
 
 @app.delete("/posts/{id}")
--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from .db import Base
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import TIMESTAMP, Boolean, Column, Integer, String, text
 
 class Post(Base):
     __tablename__ = 'posts'
@@ -7,4 +7,5 @@ class Post(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
-    published = Column(Boolean, default=True)
+    published = Column(Boolean, server_default="TRUE",nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text("now()"))
